806f8f8e68845eeb3b680eebb7551747aeb6a9cf
ShadowEngineer <ShadowEngineer54@gmail.com>
2026-02-27 14:17:16 +0000

    implement better saving and a progress meter

--- run-experiment.jl
+++ run-experiment.jl
@@ -1,5 +1,12 @@
 using Random
 using Assignment
+using JLD2
+using ProgressMeter
+using Base.Filesystem
+
+RUN_SA = true
+RUN_BGA = true
+RUN_IBGA = false
 
 problems = [
     Assignment.read_data_file("data/sppnw41.txt"),
@@ -9,43 +16,57 @@ problems = [
 
 seeds = [Random.Xoshiro(10i) for i in 1:30]
 
-solutions = Dict([
-    problem => Dict(
-        :sa => Assignment.RunResult[],
-        :bga => Assignment.RunResult[],
-        :ibga => Assignment.RunResult[]
-    ) for problem in problems
-])
-
 total_problems = length(problems)
 total_seeds = length(seeds)
 
-# simulated annealing
-for (pindex, problem) in enumerate(problems)
-    for (sindex, seed) in enumerate(seeds)
-        println("[$pindex/$total_problems] [$sindex/$total_seeds] Running simulated annealing...")
-
-        config = Assignment.SAConfig(;
-            iterations=10000,
-            penalty=5.0,
-            temperature=Assignment.SATemperatureConfig(
-                T0=100,
-                alpha=0.99,
-            ),
-            bailout=Assignment.SABailoutConfig(
-                enabled=true,
-                max_attempts=10
-            ),
-            normalised=true
-        )
-        result = Assignment.simulated_annealing(problem;
-            rng=seed,
-            settings=config,
-            verbosity=0
-        )
-
-        push!(solutions[problem][:sa], result)
+function save_sols(solutions::Dict{String,Vector{Assignment.RunResult}}, algname::String)
+    previous_files = filter(f -> isfile(f) && endswith(f, ".jld2") && contains(f, "-$algname-"), readdir(pwd()))
+    filename = "solutions-$algname-latest.jld2"
+    if length(previous_files) > 0
+        rename(filename, "solutions-$algname-$(length(previous_files)).jld2")
     end
+
+    println("Saving to: $filename")
+    JLD2.save(filename, solutions)
+end
+
+# 
+progress(desc::String) = Progress(total_seeds; dt=0.1, desc=desc, barglyphs=BarGlyphs(""))
+
+# simulated annealing
+start = time()
+if RUN_SA
+    println("Running simulated annealing...")
+    solutions_sa = Dict([problem.name => Assignment.RunResult[] for problem in problems])
+
+    for (pindex, problem) in enumerate(problems)
+        prog = progress("  SA on $(problem.name)")
+        for (sindex, seed) in enumerate(seeds)
+            config = Assignment.SAConfig(;
+                iterations=10000,
+                penalty=5.0,
+                temperature=Assignment.SATemperatureConfig(
+                    T0=100,
+                    alpha=0.99,
+                ),
+                bailout=Assignment.SABailoutConfig(
+                    enabled=true,
+                    max_attempts=10
+                ),
+                normalised=true
+            )
+            result = Assignment.simulated_annealing(problem;
+                rng=seed,
+                settings=config,
+                verbosity=0
+            )
+
+            push!(solutions_sa[problem.name], result)
+            next!(prog)
+        end
+    end
+
+    save_sols(solutions_sa, "sa")
 end
 
 # binary genetic algorithm
@@ -54,46 +75,61 @@ end
 epochs = 1000
 penalty = 10000.0
 
-for (pindex, problem) in enumerate(problems)
-    for (sindex, seed) in enumerate(seeds)
-        println("[$pindex/$total_problems] [$sindex/$total_seeds] Running binary genetic algorithm...")
+if RUN_BGA
+    solutions_bga = Dict([problem.name => Assignment.RunResult[] for problem in problems])
 
-        config = Assignment.BGAConfig(;
-            rng=seed,
-            v=0,
-            epochs=epochs,
-            mu=μ,
-            lambda=λ,
-            penalty=penalty,
-            initialisation=Assignment.BGAUniformlyRandomInitialisation(),
-            selection=Assignment.BGASelectionConfig(; k=2),
-            reproduction=Assignment.BGACombinedReproduction()
-        )
-        result = Assignment.binary_genetic_algorithm(problem; config=config)
+    for (pindex, problem) in enumerate(problems)
+        prog = progress(" BGA on $(problem.name)")
+        for (sindex, seed) in enumerate(seeds)
+            config = Assignment.BGAConfig(;
+                rng=seed,
+                v=0,
+                epochs=epochs,
+                mu=μ,
+                lambda=λ,
+                penalty=penalty,
+                initialisation=Assignment.BGAUniformlyRandomInitialisation(),
+                selection=Assignment.BGASelectionConfig(; k=2),
+                reproduction=Assignment.BGACombinedReproduction()
+            )
+            result = Assignment.binary_genetic_algorithm(problem; config=config)
 
-        push!(solutions[problem][:bga], result)
+            push!(solutions_bga[problem.name], result)
+            next!(prog)
+        end
     end
+
+    save_sols(solutions_bga, "bga")
 end
 
 # improved binary genetic algorithm
-for (pindex, problem) in enumerate(problems)
-    for (sindex, seed) in enumerate(seeds)
-        println("[$pindex/$total_problems] [$sindex/$total_seeds] Running improved binary genetic algorithm...")
+if RUN_IBGA
+    solutions_ibga = Dict([problem.name => Assignment.RunResult[] for problem in problems])
 
-        config = Assignment.BGAConfig(;
-            rng=seed,
-            v=0,
-            epochs=epochs,
-            mu=μ,
-            lambda=λ,
-            penalty=penalty,
-            initialisation=Assignment.BGAPseudoRandomInitialisation(),
-            selection=Assignment.BGASelectionConfig(; k=2),
-            reproduction=Assignment.BGAStochasticRankingReproduction(; N=lambda, P_f=0.5),
-            heuristic_improvement=true,
-        )
-        result = Assignment.binary_genetic_algorithm(problem; config=config)
+    for (pindex, problem) in enumerate(problems)
+        prog = progress("IBGA on $(problem.name)")
+        for (sindex, seed) in enumerate(seeds)
+            config = Assignment.BGAConfig(;
+                rng=seed,
+                v=0,
+                epochs=epochs,
+                mu=μ,
+                lambda=λ,
+                penalty=penalty,
+                initialisation=Assignment.BGAPseudoRandomInitialisation(),
+                selection=Assignment.BGASelectionConfig(; k=2),
+                reproduction=Assignment.BGAStochasticRankingReproduction(; N=lambda, P_f=0.5),
+                heuristic_improvement=true,
+            )
+            result = Assignment.binary_genetic_algorithm(problem; config=config)
 
-        push!(solutions[problem][:ibga], result)
+            push!(solutions_ibga[problem.name], result)
+            next!(prog)
+        end
     end
+
+    save_sols(solutions_ibga, "ibga")
 end
+finish = time()
+
+println("All simulations finished. Total time: $(finish - start)")
